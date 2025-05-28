dc77864ff6c4ce310adde98fdc022a4ca45c6382
choppei <berk.kucukogluu@gmail.com>
2025-05-28 20:37:48 +0300

    ...

--- BlogStore.DataAccessLayer/Context/BlogContext.cs
+++ BlogStore.DataAccessLayer/Context/BlogContext.cs
@@ -12,7 +12,7 @@ namespace BlogStore.DataAccessLayer.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=;initial catalog=BlogStoreNightDb;integrated Security=true;trust server certificate=true");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-KLID667\\SQLEXPRESS01;initial catalog=BlogStoreNightDb;integrated Security=true;trust server certificate=true");
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Comment> Comments { get; set; }
