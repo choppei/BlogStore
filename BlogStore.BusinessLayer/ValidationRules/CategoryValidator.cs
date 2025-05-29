5bcf8592e38361a3524129832620e172a85e17f0
choppei <berk.kucukogluu@gmail.com>
2025-05-29 22:20:16 +0300

    ...

---
+++ BlogStore.BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,20 @@
+﻿using BlogStore.EntityLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStore.BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("lütfen kategori adını giriniz");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter veri girişi yapınız!");
+            RuleFor(x => x.CategoryName).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter veri girişi yapınız!");
+        }
+    }
+}
---
+++ BlogStore.DataAccessLayer/EntityFramework/EfCommentDal.cs
@@ -0,0 +1,21 @@
+﻿using BlogStore.DataAccessLayer.Abstract;
+using BlogStore.DataAccessLayer.Context;
+using BlogStore.DataAccessLayer.Repositories;
+using BlogStore.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogStore.DataAccessLayer.EntityFramework
+{
+    public class EfCommentDal : GenericRepository<Comment>, ICommentDal
+    {
+        private readonly BlogContext _context;
+        public EfCommentDal(BlogContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
